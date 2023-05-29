90b72b11a499167690c13f28f0d97d9b882149e5
stan is love <stasoft91@gmail.com>
2023-05-29 21:17:41 +0400

    done

--- src/components/IconInput/IconInput.js
+++ src/components/IconInput/IconInput.js
@@ -13,7 +13,61 @@ const IconInput = ({
   size,
   placeholder,
 }) => {
-  return 'TODO';
+   //large == 22
+
+  return (
+      <Wrapper style={{'--width': width + 'px'}}>
+          <Input placeholder={placeholder} size={size}></Input>
+          <IconStyled id={icon} size={size === 'large' ? 24 : 16}></IconStyled>
+          <VisuallyHidden>{label}</VisuallyHidden>
+      </Wrapper>
+  );
 };
+
+const IconStyled = styled(Icon)`
+  opacity: 0.5;
+  position: absolute;
+  top: 0;
+  left: 0;
+  font-size: 0.75rem;
+`;
+
+const Input = styled.input`
+  border: none;
+  border-bottom: ${props => props.size === 'large' ? '2px' : '1px'} solid ${COLORS.black};
+  position: relative;
+  padding-left: 1.75em;
+  font-size: ${props => props.size === 'large' ? '1.25rem' : '0.75rem'};
+  color: ${COLORS.gray700};
+  font-family: 'Roboto', sans-serif;
+  width: var(--width);
+  outline-offset: 2px;
+  
+  &:not(:placeholder-shown) {
+    font-weight: 700;
+  }
+  
+  &::placeholder {
+    color: ${COLORS.gray500};
+  }
+`;
+
+const Wrapper = styled.label`
+  isolation: isolate;
+  position: relative;
+  display: block;
+
+  &:hover {
+    ${IconStyled} {
+      opacity: 1;
+    }
+    ${Input} {
+      color: ${COLORS.black};
+    }
+  }
+`;
+
+
+
 
 export default IconInput;
--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -6,13 +6,71 @@ import Icon from '../Icon';
 import { getDisplayedValue } from './Select.helpers';
 
 const Select = ({ label, value, onChange, children }) => {
-  const displayedValue = getDisplayedValue(value, children);
+    const displayedValue = getDisplayedValue(value, children);
 
-  return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
-  );
+    const IconStyled = styled(Icon)`
+      opacity: 0.5;
+    `;
+
+    const IconWrapper = styled.div`
+      position: absolute;
+      top: 0;
+      bottom: 0;
+      right: 10px;
+      margin: auto;
+      height: 24px;
+      width: 24px;
+
+    `;
+    const VisualComponent = styled.div`
+      display: block;
+      position: relative;
+      padding: 12px 52px 12px 16px;
+      border: none;
+      background: ${COLORS.transparentGray15};
+      color: ${COLORS.gray700};
+      font-family: 'Roboto', sans-serif;
+      font-size: 1rem;
+      border-radius: 8px;
+    `;
+
+    const Wrapper = styled.div`
+      position: relative;
+      width: max-content;
+      isolation: isolate;
+
+      &:hover {
+        ${IconStyled} {
+          opacity: 1;
+        }
+        ${VisualComponent} {
+          color: ${COLORS.black};
+        }
+      }
+    `;
+
+    const NativeSelect = styled.select`
+      opacity: 0;
+      position: absolute;
+      top: 0;
+      bottom: 0;
+      left: 0;
+      right: 0;
+    `;
+
+    return (
+        <Wrapper>
+            <IconWrapper>
+                <IconStyled id="chevron-down" strokeWidth={1} size={24} />
+            </IconWrapper>
+            <VisualComponent>
+                {displayedValue}
+            </VisualComponent>
+            <NativeSelect value={value} onChange={onChange}>
+                {children}
+            </NativeSelect>
+        </Wrapper>
+    );
 };
 
 export default Select;
